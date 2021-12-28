70e01609dca3d03a84646aa940a322b5b568fb55
David Lee <david.m.lee@ihwic.com>
2021-12-28 12:42:48 +0800

    Adding Image and Text and setup preview

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    VStack(alignment: .leading, spacing: 4.0) {
+      Spacer()
+      HStack {
+        Spacer()
+        Image("Illustration 1")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+        Spacer()
+      }
+      Text("SwiftUI for Multiplatform")
+        .fontWeight(.bold)
+        .foregroundColor(Color.white)
+      Text("4 Platforms")
+        .font(.footnote)
+        .foregroundColor(Color.white)
     }
+    .padding(.all)
+    .background(Color.blue)
+    .cornerRadius(20.0)
+    .shadow(radius: 10)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+    ContentView()
+      .previewLayout(.fixed(width: 200.0, height: 200.0))
+      .preferredColorScheme(.dark)
+  }
 }
