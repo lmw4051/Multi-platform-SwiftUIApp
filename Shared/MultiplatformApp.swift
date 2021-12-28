0f2392b2bfde4d853cfdef6053f2a19ad489be5a
David Lee <david.m.lee@ihwic.com>
2021-12-28 12:09:22 +0800

    Initial Commit

---
+++ Shared/MultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformApp.swift
+//  Shared
+//
+//  Created by David on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
