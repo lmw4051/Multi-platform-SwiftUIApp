699d0cff1414f41a87a6c52cd2c56c5315bb6049
David Lee <david.m.lee@ihwic.com>
2021-12-28 16:42:43 +0800

    Adding model data and transition for specific view

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -14,20 +14,20 @@ struct CoursesView: View {
   var body: some View {
     ZStack {
       ScrollView {
-        VStack {
-          CourseItem()
-            .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
+        VStack(spacing: 20) {
+          ForEach(courses) { item in
+            CourseItem(course: item)
+              .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
             .frame(width: 335, height: 250)
-          CourseItem()
-            .frame(width: 335, height: 250)
+          }
         }
         .frame(maxWidth: .infinity)
       }
       
       if show {
         ScrollView {
-          CourseItem()
-            .matchedGeometryEffect(id: "Card", in: namespace)
+          CourseItem(course: courses[0])
+            .matchedGeometryEffect(id: courses[0].id, in: namespace)
             .frame(height: 300)
           VStack {
             ForEach(0 ..< 20) { item in
